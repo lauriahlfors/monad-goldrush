dd3ea0702d19554b81f1703fbda041adf12491e9
Lauri Ahlfors <ahlfroslauri@gmail.com>
2024-02-04 21:21:15 +0200

    feat: add akvamariini color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,23 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    colors: {
+      akvamariini: {
+        50: '#E3FDF4',
+        100: '#C7FAE9',
+        200: '#8EF5D3',
+        300: '#56F0BD',
+        400: '#1EECA7',
+        500: '#10B981',
+        600: '#0D9668',
+        700: '#0A714E',
+        800: '#064B34',
+        900: '#03261A',
+        950: '#03110C',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
